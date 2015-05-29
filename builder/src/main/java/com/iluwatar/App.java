09ef2bb138e5f70d3a15fe309b60721c09ba859f
bryanshubo <bryanshubo@gmail.com>
2015-05-29 17:29:35 -0400

    modify builder pattern

--- builder/src/main/java/com/iluwatar/App.java
+++ builder/src/main/java/com/iluwatar/App.java
@@ -22,17 +22,17 @@ public class App {
 		Hero mage = new HeroBuilder(Profession.MAGE, "Riobard")
 				.withHairColor(HairColor.BLACK).withWeapon(Weapon.DAGGER)
 				.build();
-		System.out.println(mage);
+		System.out.println("mage: " + mage);
 
 		Hero warrior = new HeroBuilder(Profession.WARRIOR, "Amberjill")
 				.withHairColor(HairColor.BLOND)
 				.withHairType(HairType.LONG_CURLY).withArmor(Armor.CHAIN_MAIL)
 				.withWeapon(Weapon.SWORD).build();
-		System.out.println(warrior);
+		System.out.println("warrior: " + warrior);
 
 		Hero thief = new HeroBuilder(Profession.THIEF, "Desmond")
 				.withHairType(HairType.BALD).withWeapon(Weapon.BOW).build();
-		System.out.println(thief);
+		System.out.println("thief: " + thief);
 
 	}
 }
